9d28a023e023754d73034fb962d81ec6cea14cef
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-04-04 22:06:23 +0430

    week6 E20 solve  promebv softkeyborad overflow layer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,6 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
