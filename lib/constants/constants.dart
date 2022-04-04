f95305024c9f59dc261c64b7cb1399abf5578efa
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-04-04 14:11:34 +0430

    week67 E15 textfield widget

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,3 +3,5 @@ import 'package:hexcolor/hexcolor.dart';
 
 final Color orangeBackground = HexColor('#FF3D00');
 final Color balck = HexColor('#212121');
+
+final String developerName = 'AmirahmadAdibi';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'dart:ui';
-
+import 'package:bmi_calc/constants/constants.dart';
 import 'package:bmi_calc/widgets/background_shape_left.dart';
 import 'package:bmi_calc/widgets/background_shape_right.dart';
 import 'package:flutter/material.dart';
@@ -14,31 +13,41 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'dana'),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Text(
+            'تو چنده ؟ BMI',
+            style: TextStyle(
+              color: balck,
+            ),
+          ),
+        ),
         body: SafeArea(
-            child: Column(
-          children: [
-            RightShape(
-              widht: 180,
-            ),
-            RightShape(
-              widht: 190,
-            ),
-            RightShape(
-              widht: 210,
-            ),
-            LeftShape(
-              width: 200,
-            ),
-            LeftShape(
-              width: 210,
-            ),
-            LeftShape(
-              width: 220,
-            ),
-          ],
-        )),
+          child: Column(
+            children: [
+              Container(
+                width: 300,
+                child: TextField(
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: orangeBackground),
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'وزن',
+                    hintStyle:
+                        TextStyle(color: orangeBackground.withOpacity(0.5)),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
